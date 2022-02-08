55c7461c1d5eb05cf6aa35b2a3f7995a4f77b03d
Shivam <shivamthaker1999@gmail.com>
2022-02-08 18:51:30 +0530

    Cursor Color Updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Homepage from "./pages/Homepage";
-import Workpage from "./pages/Workpage";
-import Aboutpage from "./pages/Aboutpage";
-import Contactpage from "./pages/Contactpage";
-import Projectpage from "./pages/Projectpage";
-import Contactbar from "./components/Contactbar";
-import ImageReveal from "./components/ImageReveal";
-import Footer from "./components/Footer";
-import AnimatedCursor from "react-animated-cursor";
-import { useEffect, useState } from "react";
-import Loader from "./components/Loader";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Homepage from './pages/Homepage';
+import Workpage from './pages/Workpage';
+import Aboutpage from './pages/Aboutpage';
+import Contactpage from './pages/Contactpage';
+import Projectpage from './pages/Projectpage';
+import Contactbar from './components/Contactbar';
+import ImageReveal from './components/ImageReveal';
+import Footer from './components/Footer';
+import AnimatedCursor from 'react-animated-cursor';
+import { useEffect, useState } from 'react';
+import Loader from './components/Loader';
 
 function App() {
   const [loader, setLoader] = useState(true);
@@ -28,22 +28,15 @@ function App() {
       ) : (
         <>
           <Navbar />
-          <AnimatedCursor
-            innerSize={16}
-            outerSize={54}
-            color="10, 10, 10"
-            outerAlpha={0.2}
-            innerScale={0.7}
-            outerScale={4}
-          />
+          <AnimatedCursor innerSize={16} outerSize={54} color='244, 196, 48' outerAlpha={0.2} innerScale={0.7} outerScale={4} />
           <Contactbar />
           <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route exact path="/work" component={Workpage} />
-            <Route exact path="/about" component={Aboutpage} />
-            <Route exact path="/contact" component={Contactpage} />
-            <Route exact path="/project/:id" component={Projectpage} />
-            <Route exact path="/image" component={ImageReveal} />
+            <Route exact path='/' component={Homepage} />
+            <Route exact path='/work' component={Workpage} />
+            <Route exact path='/about' component={Aboutpage} />
+            <Route exact path='/contact' component={Contactpage} />
+            <Route exact path='/project/:id' component={Projectpage} />
+            <Route exact path='/image' component={ImageReveal} />
           </Switch>
           <Footer />
         </>
